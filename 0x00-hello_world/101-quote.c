b79add3c8082ad50fc30463bbc5df3fdf743fd65
DelGideonstar <gideonmulimutunga@gmail.com>
2022-09-12 08:42:25 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - Prints out the last part of a quote
- *
- * This is an advanced quiz
- * Return: 1
- */
-int main (void)
+ *  * main - Entry point
+ *   * Return:1
+ *    */
+int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	char ch[]
+		 = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, ch, sizeof(ch));
 	return (1);
 }
