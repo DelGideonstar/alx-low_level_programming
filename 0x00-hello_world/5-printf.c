30d9c53569a069eba9e66ab14cc4b1c8f20a8e11
DelGideonstar <gideonmulimutunga@gmail.com>
2022-09-12 07:59:23 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - printf
  *
- * Return: Always 0 (Success)
- * */
+ * Return: Always return 0
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
