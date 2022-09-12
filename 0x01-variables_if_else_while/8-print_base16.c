d93c70cb07b0ad87303c391ebef959aea060a396
DelGideonstar <gideonmulimutunga@gmail.com>
2022-09-12 15:56:42 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - start point of the formula
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int current = 48;
+
+	while (current < 58)
+	{
+		putchar(current++);
+	}
+	current += 39;
+	while (current < 103)
+	{
+		putchar(current++);
+	}
+	putchar('\n');
+	return (0);
+}
